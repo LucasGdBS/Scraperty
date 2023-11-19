156d4fc2fae3f6f19a36d7ff0be2dd24db9a2e6f
Lucas-PC <lgbs@cesar.school>
2023-11-19 00:16:52 -0300

    feat: create main file

---
+++ src/main.py
@@ -0,0 +1,20 @@
+'''
+    Script responsável por executar o scraping e 
+    filtrar os fundos de acordo com a estratégia definida.
+'''
+from modelos import Estrategia, Scrapy
+
+scrapy = Scrapy()
+
+strategy = Estrategia(
+    cotacao_atual_minima=50.0,
+    dividiend_yield_minimo=5,
+    p_vp_minimo=0.70,
+    valor_mercado_minimo=200000000,
+    liquidez_minima=50000,
+    qt_imoveis_minimo=5,
+    maxima_vacancia_media=10
+)
+
+resultado = [elemento for elemento in scrapy.scraping() if strategy.filtering(elemento)]
+print(strategy.tabulating(resultado))
